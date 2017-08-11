cce85cabf756b6eee7c1a3130f71d9b0bde3c785
ZeroKnight <xzeroknightx@gmail.com>
2017-08-11 00:48:44 -0700

    Current progress on ZeroBot::IRC

--- ZeroBot/IRC.pm
+++ ZeroBot/IRC.pm
@@ -8,15 +8,16 @@ use ZeroBot::Module -all;
 use ZeroBot::IRC::Network;
 use ZeroBot::IRC::Server;
 use ZeroBot::IRC::Message;
+use ZeroBot::IRC::Command;
 
-use Moo;
-use Types::Standard qw(HashRef InstanceOf);
-
-# use IRC::Utils qw(...);
+use IRC::Utils ':ALL';
 use POE qw(
   Component::IRC::State
   Component::IRC::Plugin::Connector
 );
+
+use Moo;
+use Types::Standard qw(HashRef InstanceOf);
 
 has networks => (
   is  => 'rw',
@@ -44,12 +45,12 @@ sub _initialize_irc
   CORE::say 'Initializing IRC Module';
 
   my $irc_cfg = ZBCORE->cfg->irc;
-  my ($available, $autoconnected);
+  my ($available, $autoconnecting);
   foreach my $network (keys %{$irc_cfg->{Networks}})
   {
     my $nethash = $irc_cfg->{Networks}{$network};
 
-    # Create Server object(s)
+    # Create ZeroBot::IRC::Server object(s)
     my @servers;
     foreach my $server (@{$nethash->{Servers}})
     {
@@ -76,17 +77,19 @@ sub _initialize_irc
       next;
     }
 
-    # Create Network object
+    # Create ZeroBot::IRC::Network object
     my %network_opts = (
       name     => $network,
       servers  => [@servers],
       channels => [ @{$nethash->{Channels}} ],
     );
 
+    # Fall back to main Identity, or even a hard default if needed
+    my $nick  = $nethash->{Nick}  // $irc_cfg->{Identity}{Nick}  // 'ZeroBot';
+    my $user  = $nethash->{User}  // $irc_cfg->{Identity}{User}  // 'zerobot';
+    my $gecos = $nethash->{Gecos} // $irc_cfg->{Identity}{Gecos} // "ZeroBot v$VERSION";
+
     # Fall back to constructor defaults if needed
-    my $nick  = $nethash->{Nick}  // $irc_cfg->{Identity}{Nick};
-    my $user  = $nethash->{User}  // $irc_cfg->{Identity}{User};
-    my $gecos = $nethash->{Gecos} // $irc_cfg->{Identity}{Gecos};
     $network_opts{nick}  = $nick if defined $nick;
     $network_opts{user}  = $user if defined $user;
     $network_opts{gecos} = $gecos if defined $gecos;
@@ -97,13 +100,17 @@ sub _initialize_irc
     # Connect any Networks set to AutoConnect
     if ($irc_cfg->{Networks}{$network}{AutoConnect})
     {
-      ++$autoconnected;
+      ++$autoconnecting;
+
+      # Actual connection is dispatched, so that POE can handle it like any
+      # other event, and so that initialization is separate from connection,
+      # which enables modules to reconnect a given network.
       module_send_event(irc_connect_network => $network);
     }
     ++$available;
   }
   # TODO: proper logging
-  CORE::say "Initialized $available Networks, $autoconnected auto-connecting";
+  CORE::say "Initialized $available Networks, $autoconnecting auto-connecting";
 }
 
 sub Bot_irc_connect_network
@@ -156,7 +163,7 @@ sub Bot_irc_connect_network
   $network_obj->set_irc($irc);
 
   # Create POE::Session for IRC Component
-  $self->_create_network_session($network);
+  $self->_create_network_session($network_obj);
   # TODO: debug log success here
 
   return MODULE_EAT_ALL;
@@ -171,44 +178,43 @@ sub _create_network_session
   # Create a session and assign callbacks to handle the IRC messages/events
   # Store the corresponding Network name in the heap
   POE::Session->create(
-    heap => { Network => $network, irc => $self->networks->{$network}->irc },
+    heap => { network => $network, irc => $network->irc },
     object_states => [
       $self => [qw(
         _start
         _stop
         irc_error
         irc_connected
+        irc_disconnected
+
         irc_join
         irc_nick
+
+        irc_302
       )],
       $self => {
         _default => 'irc_default',
         irc_001  => 'irc_welcome',
+        irc_432  => 'irc_erroneous_nickname',
         irc_433  => 'irc_nickname_in_use',
       },
     ],
     # Inlining these prevents requiring 4 nearly-identical subs or a bunch of
     # wrapper subs feeding irc_spoke
-    # inline_states => {
-    #   irc_public      => sub { $self->irc_spoke(MSGTYPE_PUBLIC,  @_) },
-    #   irc_msg         => sub { $self->irc_spoke(MSGTYPE_PRIVATE, @_) },
-    #   irc_notice      => sub { $self->irc_spoke(MSGTYPE_NOTICE,  @_) },
-    #   irc_ctcp_action => sub { $self->irc_spoke(MSGTYPE_ACTION,  @_) },
-    # },
-  ) or die "Failed to create IRC component session for Network $network";
+    inline_states => {
+      irc_public      => sub { $self->irc_spoke(MSGTYPE_PUBLIC,  @_) },
+      irc_msg         => sub { $self->irc_spoke(MSGTYPE_PRIVATE, @_) },
+      irc_notice      => sub { $self->irc_spoke(MSGTYPE_NOTICE,  @_) },
+      irc_ctcp_action => sub { $self->irc_spoke(MSGTYPE_ACTION,  @_) },
+    },
+  ) or die 'Failed to create IRC component session for Network '.$network->name."\n";
   # TODO: proper logging
 }
-
-sub _stop { ... }
-sub irc_error { ... }
-sub irc_join { ... }
-sub irc_nick { ... }
 
 sub _start
 {
   my ($self, $kernel, $heap) = @_[OBJECT, KERNEL, HEAP];
-  my $network = $heap->{Network};
-  my $irc = $heap->{irc};
+  my ($network, $irc) = @{$heap}{'network', 'irc'};
 
   # NOTE: Can register signals here (eg. DIE)
 
@@ -232,45 +238,240 @@ sub _start
   $irc->yield(connect => {});
 }
 
+sub _stop
+{
+  # TODO: not sure what we'll do here yet, but probably try to gracefully close
+  # any running IRC sessions (if they still exist at this point), and any other
+  # related cleanup
+  ...
+}
+
 sub irc_connected
 {
-  my ($self, $server) = @_[OBJECT, ARG0];
+  my ($self, $heap, $server) = @_[OBJECT, HEAP, ARG0];
+  my $network = $heap->{network};
+  my $name = $network->name;
 
   # TODO: proper logging (debug? or info?)
-  CORE::say "Established connection to $server, now registering...";
+  CORE::say "[$name] Established connection to $server, now registering...";
+}
+
+sub irc_disconnected
+{
+  my ($self, $heap, $server) = @_[OBJECT, HEAP, ARG0];
+  my ($network, $irc) = @{$heap}{'network', 'irc'};
+  my $name = $network->name;
+
+  # TODO: proper logging
+  CORE::say "[$name] Disconnected from $server";
+
+  $network->_set_connected(0);
+  module_send_event(disconnected => $network, $server);
 }
 
 sub irc_welcome
 {
   # 001 RPL_WELCOME
   my ($self, $heap) = @_[OBJECT, HEAP];
-  my $network = $heap->{Network};
-  my $irc = $heap->{irc};
+  my ($network, $irc) = @{$heap}{'network', 'irc'};
+  my $name = $network->name;
 
   # TODO: proper logging
   my $server = $irc->server_name;
-  CORE::say "Network $network: Successfully connected to $server";
+  CORE::say "Network $name: Successfully connected to $server";
+  $network->_set_connected(1);
+  $network->_set_connected_at(time);
+
+  # Request our Username and Host from the server, as it may have mangled our
+  # Username (Freenode prefixing '~' on non-ident clients), or a cloak may have
+  # been applied over our Host (umode +x or otherwise).
+  $irc->yield(userhost => $network->nick);
+
+  # TODO: send 'connected' event
+  # module_send_event(connected => ...);
 
   # Join configured channels
-  foreach my $channel (@{$self->networks->{$network}->channels})
+  foreach my $channel (@{$network->channels})
   {
-    CORE::say "Joining $channel";
+    CORE::say "[$name] Joining $channel";
     $irc->yield(join => $channel);
   }
 }
 
-sub irc_nickname_in_use
+sub irc_join
 {
-  # 443 ERR_NICKNAMEINUSE
-  my ($self, $heap) = @_[OBJECT, HEAP];
-  my $network = $heap->{Network};
-  my $network_obj = $self->networks->{$network};
-  my $irc = $heap->{irc};
+  my ($self, $heap, $who, $channel) = @_[OBJECT, HEAP, ARG0, ARG1];
+  my ($network, $irc) = @{$heap}{'network', 'irc'};
+  my $nick = parse_user($who);
+
+  # TODO: send 'join' event
+  # module_send_event(join => ...);
+}
+
+sub irc_nick
+{
+  my ($self, $heap, $who, $newnick, $common) = @_[OBJECT, HEAP, ARG0 .. ARG2];
+  my ($network, $irc) = @{$heap}{'network', 'irc'};
+
+  $network->_set_nick($newnick) if parse_user($who) eq $irc->nick_name; # XXX: temp
+
+  # TODO: send nick event to modules
+  # module_send_event(nick_change => data...);
+}
+
+sub irc_spoke
+{
+  my ($self, $msgtype) = splice @_, 0, 2;
+  my ($heap, $src, $dests, $body) = @_[HEAP, ARG0 .. ARG2];
+  my ($network, $irc) = @{$heap}{'network', 'irc'};
+  my $cmdchar = ZBCORE->cmdchar;
+
+  # Determine whether this is a plain message, or a command
+  if ($msgtype < MSGTYPE_NOTICE and $body =~ /^$cmdchar/)
+  {
+    # module_send_event(commanded => $body);
+    # ...
+    # XXX: temp test
+    my $cmd = ZeroBot::Command->new(
+      line => $body,
+      spec => {
+        test => {
+          'h|help|fuck' => OPTVAL_NONE,
+          'f|foo' => OPTVAL_NONE,
+          'b|bar|barr' => OPTVAL_REQUIRED,
+          'l|log' => OPTVAL_OPTIONAL,
+        }
+      }
+    );
+    {
+      no warnings;
+      my @m = ('Name:', $cmd->name, 'Args:', @{$cmd->args}, 'Opts:', %{$cmd->opts});
+      $irc->yield(privmsg => $dests->[0], "@m");
+    }
+  }
+  else
+  {
+    use feature 'switch';
+    no warnings 'experimental::smartmatch';
+    my $msg = ZeroBot::IRC::Message->new(
+      network => $network,
+      src     => $src,
+      dests   => $dests,
+      type    => $msgtype,
+      message => $body,
+    );
+    # for ($msgtype)
+    # {
+    #   module_send_event(public  => $msg) when MSGTYPE_PUBLIC;
+    #   module_send_event(private => $msg) when MSGTYPE_PRIVATE;
+    #   module_send_event(notice  => $msg) when MSGTYPE_NOTICE;
+    #   module_send_event(action  => $msg) when MSGTYPE_ACTION;
+    # }
+    # ...
+  }
+}
+
+sub irc_302
+{
+  # 302 RPL_USERHOST
+  my ($self, $heap, $rpl) = @_[OBJECT, HEAP, ARG1];
+  my ($network, $irc) = @{$heap}{'network', 'irc'};
+
+  # NOTE: The '+' or '-' indicates away state; '-' means away.
+  my ($nick, $user, $host) = $rpl =~ /([^=]+)=[+-]([^@]+)@(\S+)/;
+
+  # Keep our user/host attributes in sync
+  if ($nick eq $network->nick)
+  {
+    $network->_set_user($user);
+    $network->_set_host($host);
+  }
+}
+
+### Error events ###############################################
+
+sub irc_error
+{
+  my ($self, $heap, $errmsg) = @_[OBJECT, HEAP, ARG0];
+  my ($network, $irc) = @{$heap}{'network', 'irc'};
 
   # TODO: proper logging
-  CORE::say "Nick '" . $network_obj->nick . "' already in use.";
-  $network_obj->nick($network_obj->nick . '_');
-  $irc->yield(nick => $network_obj->nick);
+  CORE::say "Network $network->name error: $errmsg";
+  module_send_event(server_error => $errmsg);
+}
+
+sub irc_erroneous_nickname
+{
+  # 432 ERR_ERRONEUSNICKNAME
+  my ($self, $heap, $err) = @_[OBJECT, HEAP, ARG2];
+  my ($network, $irc) = @{$heap}{'network', 'irc'};
+  my $badnick = $err->[0];
+
+  # TODO: proper logging
+  CORE::say "[$network->name] Erroneous Nickname: $badnick";
+
+  # Let any intersted modules handle this event
+  # NOTE: Not sure yet how we'll handle this event return here
+  my $eat = module_send_event(nick_erroneous => $badnick);
+  return if $eat == MODULE_EAT_ALL;
+
+  my $nicklen = $irc->isupport('NICKLEN');
+  if (defined $nicklen and length $badnick > $nicklen)
+  {
+    # TODO: proper logging (debug?)
+    CORE::say "[$network->name] Truncating nick to fit NICKLEN";
+    $irc->yield(nick => substr($badnick, 0, $nicklen));
+  }
+  else
+  {
+    # If we're in the registration phase with a bad nick, disconnect and let the
+    # user correct it. Otherwise this nick change attempt was likely from some
+    # bot event and we can just ignore it and move on.
+    if (!$network->connected)
+    {
+      # TODO: proper logging
+      CORE::say "[$network->name] Cannot register connection with erroneous nickname; disconnecting.";
+      $irc->disconnect;
+    }
+  }
+}
+
+# TODO: handle 437 (unavailable resource)
+sub irc_nickname_in_use
+{
+  # 433 ERR_NICKNAMEINUSE
+  my ($self, $heap) = @_[OBJECT, HEAP];
+  my ($network, $irc) = @{$heap}{'network', 'irc'};
+  my $nick = $network->nick;
+
+  # TODO: proper logging
+  CORE::say "[$network->name] Nick '$nick' already in use.";
+
+  # If we're in the registration phase, this event is either due to a ghosted
+  # connection with this nick, or the nick is has been stolen (or perhaps the
+  # other way around ;)) For now, do what we can to register and let any
+  # interested modules take over.
+  if (!$network->connected)
+  {
+    my $newnick;
+    if ($nick =~ /__$/) # Don't get crazy with the underscores
+    {
+      $newnick = substr $nick, 0, -1;
+      $newnick .= '1';
+    }
+    elsif ($nick =~ /_(\d)$/)
+    {
+      $newnick = substr $nick, 0, -1;
+      $newnick .= $1 + 1;
+    }
+    else
+    {
+      $newnick = "${nick}_";
+    }
+    $network->_set_nick($newnick);
+    $irc->yield(nick => $newnick);
+  }
+  module_send_event(nick_in_use => $nick);
 }
 
 sub irc_default
@@ -292,115 +493,3 @@ sub irc_default
 
 1;
 
-__END__
-##### OLD SHIT
-
-sub irc_spoke
-{
-  # Handles PRIVMSGs, NOTICEs and ACTIONs
-  my ($self, $msgtype) = (shift, shift);
-  my ($who, $where, $what) = @_[ARG0 .. ARG2];
-  my ($nick, $user, $host) = (split /[!@]/, $who);
-  my $cmdchar = $self->CmdChar;
-
-  # Store message info
-  my $msg = {
-    nick  => $nick,
-    user  => $user,
-    host  => $host,
-    where => $msgtype eq 'msg' ? $nick : $where->[0],
-    body  => $what,
-  };
-
-  # Are we being issued a command?
-  if ($msgtype ne 'action' and $what =~ /^$cmdchar/) {
-    $self->_parse_command($what);
-    my @arg = @{ $self->_cmdhash->{arg} };
-
-    # Looking for help...
-    if ($self->_cmdhash->{name} eq 'help') {
-      my @modules = $self->module_list(', ');
-      unless ($arg[0]) {
-        $self->reply($msg->{where}, $msg->{nick},
-          "Specify a module to see help for. Loaded modules: @modules"
-        );
-        return;
-      }
-      # Is the requested module loaded?
-      my $search = (grep { $arg[0] =~ /$_/i } keys $self->Modules)[0];
-      if ($search) {
-        $self->notice($nick => $_) for $self->Modules->{$search}->help();
-        return;
-      } else {
-        $self->reply($msg->{where}, $msg->{nick},
-          "No module named '$arg[0]' is loaded. Loaded modules: @modules"
-        );
-        return;
-      }
-    } else { # Command issued
-      my $rv;
-      foreach my $module (values $self->Modules) {
-        next unless $module->can('commanded');
-        $rv = $module->commanded($msg, $self->_cmdhash);
-        last if $rv;
-      }
-      unless ($rv) {
-        if (exists $self->Modules->{BadCmd}) {
-          $self->Modules->{BadCmd}->badcmd($msg->{where});
-        } else {
-          $self->reply($msg->{where}, $msg->{nick}, 'No such command.');
-        }
-      }
-    }
-  } else { # No command, just chatter
-    foreach my $module (values $self->Modules) {
-      if ($msgtype eq 'channel' or $msgtype eq 'msg') {
-        next unless $module->can('said');
-        $module->said($msg);
-      } elsif ($msgtype eq 'action') {
-        next unless $module->can('emoted');
-        $module->emoted($msg);
-      } elsif ($msgtype eq 'notice') {
-        next unless $module->can('noticed');
-        $module->noticed($msg);
-      }
-    }
-  }
-}
-
-sub irc_join
-{
-  my ($self, $who, $where) = @_[OBJECT, ARG0, ARG1];
-  my $irc = $self->_ircobj;
-  my $nick = (split /!/, $who)[0];
-
-  # XXX: Get our User/Hostname (PoCoIRC should have a function for this...)
-  # TODO: Update this when our user/hostname changes (vhost, etc)
-  if ($nick eq $self->Nick) {
-    $self->User((split /!/, $who)[1]); # in case the server mangled it (freenode)
-    $self->Hostname((split /@/, $who)[1]);
-  }
-
-  foreach my $module (values $self->Modules) {
-    next unless $module->can('joined');
-    $module->joined($nick, $where);
-  }
-  return;
-}
-
-sub irc_nick
-{
-  my ($self, $who, $newnick) = @_[OBJECT, ARG0, ARG1];
-
-  $self->Nick($newnick) if (split /!/, $who)[0] eq $self->Nick;
-}
-
-sub irc_stop
-{
-  return;
-}
-
-sub irc_error
-{
-  return;
-}
