7abadafb7dc78e714270d5929ea8daaeb7aa8984
ZeroKnight <xzeroknightx@gmail.com>
2017-10-30 15:09:15 -0700

    Add templated message formatting
    Includes a new utility function 'tsprintf()', which takes a string much
    like the printf family, and a hash or hashref whose keys are variable
    names that will be expanded to its value in the template string.
    Variables in the template string are prefixed with '%' with optional
    surrounding braces. Variables in the template string that are not
    defined in the hash(ref) are ignored and returned as a literal part of
    the string.

---
+++ ZeroBot/Util.pm
@@ -0,0 +1,38 @@
+package ZeroBot::Util;
+
+use Carp;
+
+use parent 'Exporter::Tiny';
+our @EXPORT    = qw();
+our @EXPORT_OK = qw(tsprintf);
+our %EXPORT_TAGS = ();
+
+sub tsprintf
+{
+  my $format = shift;
+  my %template;
+  return '' unless $format;
+
+  # Ensure we have a template to work with
+  if ($@ > 1)
+  {
+    # Given a hash
+    %template = @_;
+  }
+  else
+  {
+    confess 'tsprintf() expects a hash or hashref' unless ref $_[0] eq 'HASH';
+    %template = %{$_[0]};
+  }
+
+  my $expand = sub {
+    my ($orig, $var) = @_;
+    return exists $template{$var} ? $template{$var} : $orig;
+  };
+
+  $format =~ s/((?<!%)%\{?(\w+)\}?)/$expand->($1, $2)/ge;
+  $format =~ s/%%/%/g;
+  return $format;
+}
+
+1;
